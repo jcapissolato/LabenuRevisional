c0077ec04e319aabc8e91b183816ab263fa007e8
Jorge <jcapissolat@gmail.com>
2023-01-10 16:45:01 -0300

    exercicios node package.json

---
+++ backend/node-package/template-node-npm/search.js
@@ -0,0 +1,35 @@
+// Exercício 1
+
+// a)
+// primeiro criamos o package.json, depois acessamos através do script
+
+// b)
+// const valorTerminal = process.argv[2]
+// const valorTerminal2 = process.argv[3]
+// console.log("Olá,",valorTerminal,"! Você tem", valorTerminal2,"anos.")
+
+// c)
+// const valorTerminal = process.argv[2]
+// const valorTerminal2 = process.argv[3]
+// const valor = Number(valorTerminal2) + 7
+// console.log("Olá,",valorTerminal,"! Você tem",valorTerminal2," anos. Em sete anos você terá",valor)
+
+// Exercício 2
+
+// const valorUm = process.argv[2];
+// const valorDois = process.argv[3];
+
+// const add = Number(valorUm) + Number(valorDois);
+// const sub = Number(valorUm) - Number(valorDois);
+// const mult = Number(valorUm) * Number(valorDois);
+// const div = Number(valorUm) / Number(valorDois);
+
+// console.log(mult);
+
+//Exercício 3
+
+// const listaDeTarefas = []
+// const addTarefa = listaDeTarefas.push(process.argv[2])
+
+
+// console.log(listaDeTarefas)
