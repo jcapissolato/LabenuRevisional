6838cf83b6dd3363d54be0ca4c88bfd6c226ca77
Jorge <jcapissolat@gmail.com>
2023-01-30 16:15:23 -0300

    introdução api/express

--- backend/intro-express/build/index.js
+++ backend/intro-express/build/index.js
@@ -1,5 +1,5 @@
 "use strict";
-//Exercício 1
+// //Exercício 1
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -9,37 +9,61 @@ const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
-app.get('/iniciar-express', (request, response) => {
-    response.send("oi express");
-});
-app.get('https://jsonplaceholder.typicode.com/users', (request, response) => {
-    const users = [];
-    response.status(200).send({ message: users });
-});
-const usuarios = [
+const users = [
     {
-        nome: 'Jorge',
-        idade: 30,
-        sexo: 'masculino'
+        id: 1,
+        name: "Jorge Pissolato",
+        email: "jcapissolato@example.com",
+        posts: [
+            {
+                id: 1,
+                title: "Post 1",
+                content: "Conteúdo do post 1",
+            },
+            {
+                id: 2,
+                title: "Post 2",
+                content: "Conteúdo do post 2",
+            },
+        ],
     },
     {
-        nome: 'Regina',
-        idade: 70,
-        sexo: 'feminino'
-    },
-    {
-        nome: 'wislaine',
-        idade: 25,
-        sexo: 'feminino'
+        id: 2,
+        name: "Wislaine",
+        email: "Wisfernandes@example.com",
+        posts: [
+            {
+                id: 3,
+                title: "Post 3",
+                content: "Conteúdo do post 3",
+            },
+            {
+                id: 4,
+                title: "Post 4",
+                content: "Conteúdo do post 4",
+            },
+        ],
     },
 ];
-app.get('/usuarios', (request, response) => {
-    response.send(usuarios);
+app.get("/posts", (req, response) => {
+    let todosPosts = [];
+    users.forEach((user) => {
+        todosPosts = todosPosts.concat(user.posts);
+    });
+    response.json(todosPosts);
 });
-app.get('https://jsonplaceholder.typicode.com/posts', (request, response) => {
-    const posts = [];
-    response.status(200).send({ message: posts });
+app.get('/users/posts/:userId', (req, res) => {
+    const userId = parseInt(req.params.userId);
+    const user = users.find(user => user.id === userId);
+    if (!user) {
+        return res.status(404).json({ message: "Usuário não encontrado." });
+    }
+    res.json(user.posts);
 });
+// A criação de posts dentro do array de usuários pode ser útil para manter as informações relacionadas
+// juntas e evitar a necessidade de consultar outra fonte de dados para obter informações sobre o autor
+// de cada post. Além disso, isso pode ser útil para aplicativos que exigem uma lógica de acesso aos
+// dados mais simples.
 app.listen(3003, () => {
-    console.log('Servidor está rodando em http://localhost:3003');
+    console.log("Servidor está rodando em http://localhost:3003");
 });
