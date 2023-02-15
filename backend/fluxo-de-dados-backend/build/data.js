f49cd0032c1fc4cd2c9ffd688c66c02af3989883
Jorge <jcapissolat@gmail.com>
2023-02-15 15:20:25 -0300

    apis-rest

---
+++ backend/fluxo-de-dados-backend/build/data.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.produtos = void 0;
+exports.produtos = [
+    {
+        id: 'aaa',
+        name: 'barra olimpica',
+        price: 1500
+    },
+    {
+        id: 'bbb',
+        name: 'kettlebell',
+        price: 500
+    },
+    {
+        id: 'ccc',
+        name: 'dumbell',
+        price: 1200
+    }
+];
+//# sourceMappingURL=data.js.map
